03bb1c707d6a1e0417261176af44047c6e71f175
Josh Dawson <joshd@wson.me>
2025-02-17 20:58:47 +0000

    Change to flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,10 @@
-module.exports = {
-    overrides: [
-      {
-        files: ['*.js'],
-        rules: {
-          // Your ESLint rules for JavaScript files
-        },
-      },
-    ],
-  };
-  
+import { defineConfig } from 'eslint';
+
+export default defineConfig([
+  {
+    files: ['static/js/**/*.js'],
+    rules: {
+      // Your JavaScript-specific rules go here
+    },
+  },
+]);
